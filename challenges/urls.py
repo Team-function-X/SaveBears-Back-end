c7fae6c151f45b3c92cdcd557198bf1cdde872a6
Chanjongp <pcj0198@daum.net>
2021-05-22 03:35:41 +0900

    Feat : 사진 분석 JSON 가져오기 API

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,6 +1,7 @@
+from django.urls.conf import path
 from .views import *
 
 
 urlpatterns = [
-
+    path("image-data", GetChallengeImage.as_view(), name="image_data")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import views, status
+import requests
 
-# Create your views here.
+
+class GetChallengeImage(views.APIView):
+    """
+    AWS Lamda 서버로부터 사진분석 결과를 가져옴
+    """
+
+    def get(self, request, a):
+        print(a)
+        print("1")
+        result = requests.get(
+            f"https://th5a2rg7k4.execute-api.ap-northeast-2.amazonaws.com/api/test.jpeg")
+        result_json = result.json()
+        return JsonResponse(result_json, safe=False)
